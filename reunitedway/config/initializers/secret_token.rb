18673f15d3d03dac17fedbd7348adba416e7ffdc
Eric VanWieren <evanwieren@gmail.com>
2013-11-12 09:07:27 -0800

    Initial rails commit

---
+++ reunitedway/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reunitedway::Application.config.secret_key_base = 'ddb62ad4bfe60b1ed63cfcf15d65b037fae4e9b6dc08f37aac00935b75acaa3b449ccf5bf72124d1abbc3a33fe02daa5860480348b740028da048805a2d0ad25'
