09d5d491314632fe444f74d0048f8704d8b35d91
Jay Stramel <jaystramel@dolphinmicro.com>
2013-11-12 13:04:08 -0800

    Adding basic map search functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Reunitedway::Application.routes.draw do
   get "static_page/index"
-  get "search" => "search#index"
+  get "search"            => "search#index"
+  get "search/locations"  => "search#locations"
 
   root to: 'static_page#index'
 end
