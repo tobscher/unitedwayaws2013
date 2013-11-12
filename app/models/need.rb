c58703ef23c65f036784fdefa001bb2b6f204634
Ryan Bond <ryan@lightspeedsystems.com>
2013-11-12 10:32:27 -0800

    mongoid needs and user

---
+++ app/models/need.rb
@@ -0,0 +1,36 @@
+class Need
+
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title
+  field :description
+  field :agency_id, type: Integer
+  field :agency_name
+  field :category
+
+  field :address
+  field :city
+  field :state
+  field :zip_code
+  field :phone
+  field :email
+
+  field :loc, :type => Array
+
+  index(
+      [
+          [:loc, Mongo::GEO2D]             
+      ], background: true
+
+  )
+  index(
+    [
+      :category
+    ], background: true
+  )
+  index(
+    [ :agency_id ], background: true
+  )
+
+end
